c9f5b98dc6a6b91cab3383386f5567e67f7d5bee
Adisoftech <7589104842hi@gmail.com>
2024-05-11 02:12:27 +0530

    Added service cards

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './Components/Home';
 import About from './Components/About';
 import Services from './Components/Service';
 import { loadUser } from './action/userAction';
-import { useEffect, useState } from 'react';
 import store from "./store";
 import Contact from './Components/Contact';
-import { useSelector } from 'react-redux';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Login from './Components/UserAuth/Login'
 import Register from './Components/UserAuth/Register';
+import Service from './Components/Services';
+import AddService from './Admin/Addservice'
+import AllServices from './Admin/Allservices/Allservice';
 
 function App() {
-  const { isAuthenticated, user } = useSelector((state) => state.user);
+  const location = useLocation();
+  const isAuthPage = location.pathname === '/login' || location.pathname === '/register';
+
   useEffect(() => {
     try {
       store.dispatch(loadUser());
@@ -23,20 +26,26 @@ function App() {
     }
   }, []);
 
+  // Determine if the header and footer should be displayed
+  const showHeaderFooter = !isAuthPage;
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      {/* Conditionally render Header */}
+      {showHeaderFooter && <Header />}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin/addservice" element={<AddService/>}/>
+        <Route path="/admin/products" element={<AllServices/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product" element={<Service />} />
+      </Routes>
+      {/* Conditionally render Footer */}
+      {showHeaderFooter && <Footer />}
     </div>
   );
 }
