b63699664588eb5689c4666dc4e77cb2987fab56
AyushKumar170621 <ayushk1701@gmail.com>
2024-05-13 17:07:51 +0530

    issue resolved

--- my-app/src/Components/Booking/PaymentSuccessfull.jsx
+++ my-app/src/Components/Booking/PaymentSuccessfull.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import { createBooking } from "../../action/bookingAction";
@@ -7,19 +7,22 @@ import "react-toastify/dist/ReactToastify.css";
 
 const PaymentSuccessfull = () => {
   const style = {
-    width: '200px',
-    margin: 'auto',
-    backgroundColor: '#CCC',
+    width: "200px",
+    margin: "auto",
+    backgroundColor: "#CCC",
   };
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { error,loading } = useSelector((state) => state.newBooking);
+  const { error, loading } = useSelector((state) => state.newBooking);
   const [searchParams] = useSearchParams();
   const [bookItems] = useState(JSON.parse(localStorage.getItem("bookingInfo")));
-  const [serviceItems] = useState(JSON.parse(localStorage.getItem("serviceItems")));
+  const [serviceItems] = useState(
+    JSON.parse(localStorage.getItem("serviceItems"))
+  );
   const [bookingCreated, setBookingCreated] = useState(false);
-  const [counter,setCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const bookingCreatedRef = useRef(false);
 
   useEffect(() => {
     const booking = {
@@ -35,32 +38,40 @@ const PaymentSuccessfull = () => {
     };
 
     // Check if the booking has already been created
-    if (!error && !bookingCreated && !loading) {
-      dispatch(createBooking(booking));
+    if (!error && !bookingCreatedRef.current && !loading) {
+      dispatch(createBooking(booking))
+        .then((result) => {
+          console.log(result);
+          if(result.success === true)
+            {
+              toast.success("Booked Successfully", {
+                autoClose: 2000,
+                position: toast.POSITION.TOP_CENTER,
+                onClose: () => {
+                  navigate("/");
+                },
+              });
+            }
+        })
+        .catch((err) => {
+          toast.error("Booking failed! Retry later.",{
+            autoClose: 2000,
+                position: toast.POSITION.TOP_CENTER,
+                onClose: () => {
+                  navigate("/");
+                },
+              });
+        });
       console.log("trigged");
-      setBookingCreated(true); // Set a flag to indicate that the booking has been created
+      bookingCreatedRef.current = true;
+      // setBookingCreated(true); // Set a flag to indicate that the booking has been created
     }
-  }, [error,loading,bookingCreated,bookItems,serviceItems]);
+  }, [error, loading, bookItems, serviceItems]);
 
-  useEffect(() => {
-    // Handle success or error
-    if (error) {
-      window.alert(error);
-      navigate("/");
-    } else if (bookingCreated) {
-      toast.success("Booked Successfully", {
-        autoClose: 2000,
-        position: toast.POSITION.TOP_CENTER,
-        onClose: () => {
-          navigate("/");
-        }
-      });
-    }
-  }, [error, bookingCreated, navigate]);
 
   return (
     <div style={style}>
-      <ToastContainer/>
+      <ToastContainer />
       Payment Successful
     </div>
   );
